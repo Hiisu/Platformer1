35b394ad2381ac8c40bed9b5d529d0d9af03bf67
Heath Higgins <heath.higgins@gmail.com>
2013-04-13 13:11:11 -0700

    Initial texture packs and working platforming on trees with skybox.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,58 +6,63 @@ public class PlayerMovement : MonoBehaviour {
 	CharacterController controller;
 	float horizontalMovementSpeed = 5.0f;
 	
-	public float speed = 8.0F;
+	public float speed = 3.0F;
     public float jumpSpeed = 10.0F;
-    public float gravity = 20.0F;
+    public float gravity = 10.0F;
     private Vector3 moveDirection = Vector3.zero;
-	
-	public float Acceleration = 10;
-	public float MaxVelocity = 700;
-	public float SlowToFastVelocity = 600;
-	public float BrakingVelociy = 18;
-	
-	public enum PlayerState
-		{
-			Starting,
-			Standing,
-			MovingRight,
-			FastMovingRight,
-			KnockedBack,
-			Jumping,
-			Killed	
-		};
+	private bool can_climb = false;
 
-   private PlayerState state;
-	
 	// Use this for initialization
 	void Start () 
 	{
-		controller = GetComponent<CharacterController>();
-	
-			
-		state = PlayerState.Standing;
+		controller = GetComponent<CharacterController>();		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		
 		if (controller.isGrounded) 
 		{
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = base.transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
 			
-            moveDirection = base.transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-			
-            if (Input.GetKeyDown(KeyCode.Space))
-			{
-                moveDirection.y = jumpSpeed;
-			}
 		}
 		
-				
-		moveDirection.y -= gravity * Time.deltaTime;
+		
+			
+		if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+		{
+            moveDirection.y = jumpSpeed;
+		}
+		else if (can_climb && Input.GetKey(KeyCode.C))
+		{
+			moveDirection.x = Input.GetAxis("Horizontal") * speed;
+			moveDirection.y = jumpSpeed;	
+			
+		}
+		else
+		{
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+		
         controller.Move(moveDirection * Time.deltaTime);
 		
 	}
+	
+	void OnTriggerEnter()
+	{
+		can_climb = true;
+	}
+	
+	
+	
+	void OnTriggerExit()
+	{
+		can_climb = false;
+		
+	}
+	
 	
 }

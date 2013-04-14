06bf12121cf9b2becfbda3b1828b1d724c55ed1e
Heath Higgins <heath.higgins@gmail.com>
2013-04-13 22:06:30 -0700

    Minor scene tweaks.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,9 +6,10 @@ public class PlayerMovement : MonoBehaviour {
 	CharacterController controller;
 	float horizontalMovementSpeed = 5.0f;
 	
-	public float speed = 3.0F;
-    public float jumpSpeed = 10.0F;
-    public float gravity = 10.0F;
+	public float speed = 10.0F;
+    public float jumpSpeed = 20.0F;
+	public float defy_init_vector = 3.0F;
+    public float gravity = 18.0F;
     private Vector3 moveDirection = Vector3.zero;
 	private bool can_climb = false;
 
@@ -21,25 +22,34 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		
 		if (controller.isGrounded) 
 		{
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection = base.transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
 			
-		}
-		
-		
+			if (Mathf.Abs(moveDirection.y) > 0.2)
+			{
+				animation.CrossFade("run");
+			}
+			else
+			{
+				animation.CrossFade("idle");
+			}
 			
-		if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+		}
+			
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
             moveDirection.y = jumpSpeed;
+			animation.CrossFade("jump_pose");
 		}
 		else if (can_climb && Input.GetKey(KeyCode.C))
 		{
-			moveDirection.x = Input.GetAxis("Horizontal") * speed;
-			moveDirection.y = jumpSpeed;	
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = base.transform.TransformDirection(moveDirection);
+			moveDirection *= defy_init_vector;
+			moveDirection.y = jumpSpeed;
 			
 		}
 		else
